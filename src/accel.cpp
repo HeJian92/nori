2b27dacb9789367b619856a65f1e618325585a80
hjn6783 <hjn6783@corp.netease.com>
2021-09-17 14:11:50 +0800

    作业2还是去掉子节点排序比较快

--- src/accel.cpp
+++ src/accel.cpp
@@ -259,36 +259,25 @@ bool Accel::traversalIntersect(const Node& node, Ray3f& ray, Intersection& its, 
     }
     if (node.child)
     {
-        //################## Improved ray traversal ################
-        //建立<childIdx, childNodeBBoxToRayDistance>
-        std::vector<std::pair<int, float>> childToRayDistances(8);
-        for (int i = 0; i < 8; i++)
-        {
-            Node* childNode = node.child[i];
-            float distance = std::numeric_limits<float>::max();
-            if (childNode)
-            {
-                distance = childNode->bbox.distanceTo(ray.o);
-            }
-            childToRayDistances[i] = std::make_pair(i, distance);
-        }
-        sort(childToRayDistances.begin(), childToRayDistances.end(), sortChildToRayDistance);
-        //按到光线起始点距离从近到远遍历
-        for (int i = 0; i < 8; i++)
-        {
-            int childIndex = childToRayDistances[i].first;
-            Node* childNode = node.child[childIndex];
-            if (childNode)
-            {
-                foundIntersection = traversalIntersect(*childNode, ray, its, shadowRay, hit_idx) || foundIntersection;
-            }
-            if (shadowRay && foundIntersection)
-                return true;
-        }
-        ////################## ray traversal ################
+        ////################## Improved ray traversal ################
+        ////建立<childIdx, childNodeBBoxToRayDistance>
+        //std::vector<std::pair<int, float>> childToRayDistances(8);
         //for (int i = 0; i < 8; i++)
         //{
         //    Node* childNode = node.child[i];
+        //    float distance = std::numeric_limits<float>::max();
+        //    if (childNode)
+        //    {
+        //        distance = childNode->bbox.distanceTo(ray.o);
+        //    }
+        //    childToRayDistances[i] = std::make_pair(i, distance);
+        //}
+        //sort(childToRayDistances.begin(), childToRayDistances.end(), sortChildToRayDistance);
+        ////按到光线起始点距离从近到远遍历
+        //for (int i = 0; i < 8; i++)
+        //{
+        //    int childIndex = childToRayDistances[i].first;
+        //    Node* childNode = node.child[childIndex];
         //    if (childNode)
         //    {
         //        foundIntersection = traversalIntersect(*childNode, ray, its, shadowRay, hit_idx) || foundIntersection;
@@ -296,6 +285,17 @@ bool Accel::traversalIntersect(const Node& node, Ray3f& ray, Intersection& its, 
         //    if (shadowRay && foundIntersection)
         //        return true;
         //}
+        //################## ray traversal ################
+        for (int i = 0; i < 8; i++)
+        {
+            Node* childNode = node.child[i];
+            if (childNode)
+            {
+                foundIntersection = traversalIntersect(*childNode, ray, its, shadowRay, hit_idx) || foundIntersection;
+            }
+            if (shadowRay && foundIntersection)
+                return true;
+        }
     }
     return foundIntersection;
 }
